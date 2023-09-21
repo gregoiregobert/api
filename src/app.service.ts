a31cdadeeeb76cdafe66aa957dc37b387c1f8409
Gregoire GOBERT <ggobert@e1r1p9.clusters.42paris.fr>
2023-09-21 15:31:50 +0200

    GIT OK

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): Object {
-    return {title: 'Helloo Time!'};
+    return {title: 'HelloOo Time!'};
   }
 }
